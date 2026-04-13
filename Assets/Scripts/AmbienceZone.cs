3911e8aaed85865b7c07dd1a0492f318b54f29e6
ollyplumbridge <ollyplumbridge@gmail.com>
2026-04-13 18:16:50 +0200

    Bird trigger not working

--- Assets/Scripts/AmbienceZone.cs
+++ Assets/Scripts/AmbienceZone.cs
@@ -4,42 +4,44 @@ public class AmbienceZone : MonoBehaviour
 {
     public AudioSource ambience;
 
+    private Transform cam;
+
     private void Start()
     {
+        cam = Camera.main.transform;
         ambience.volume = 0f;
         ambience.Play();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.CompareTag("Player"))
+        // Check if camera is inside collider bounds
+        Collider col = GetComponent<Collider>();
+
+        if (col.bounds.Contains(cam.position))
         {
             StopAllCoroutines();
-            StartCoroutine(FadeAudio(ambience, 1f, 1f));
+            StartCoroutine(FadeAudio(1f, 1f));
+        }
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeAudio(0f, 1f));
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    System.Collections.IEnumerator FadeAudio(float targetVolume, float duration)
     {
-        if (other.CompareTag("Player"))
-        {
-            StopAllCoroutines();
-            StartCoroutine(FadeAudio(ambience, 0f, 1f));
-        }
-    }
-
-    System.Collections.IEnumerator FadeAudio(AudioSource audio, float targetVolume, float duration)
-    {
-        float startVolume = audio.volume;
+        float startVolume = ambience.volume;
         float time = 0f;
 
         while (time < duration)
         {
             time += Time.deltaTime;
-            audio.volume = Mathf.Lerp(startVolume, targetVolume, time / duration);
+            ambience.volume = Mathf.Lerp(startVolume, targetVolume, time / duration);
             yield return null;
         }
 
-        audio.volume = targetVolume;
+        ambience.volume = targetVolume;
     }
 }
